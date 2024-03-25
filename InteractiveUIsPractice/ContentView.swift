8bf2ae409a6b04f8eaedf725d88163fe0b166479
Marnie Reid <marnie@kodewithklossy.com>
2024-03-25 14:37:52 -0400

    Complete Practice

--- InteractiveUIsPractice/ContentView.swift
+++ InteractiveUIsPractice/ContentView.swift
@@ -8,14 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var name = ""
+    @State var zodiacSign = ""
+    @State private var birthday = Date()
+    @State private var presentAlert = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Example App")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            Text("What is your name?")
+                .font(.title)
+            
+            TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+                .disableAutocorrection(true)
+            
+            Spacer()
+            
+            Text("What is your birthday?")
+                .font(.title)
+            
+            //Spicy Challenge
+            DatePicker(
+                "Select Your Birthday: ",
+                selection: $birthday,
+                displayedComponents: [.date]
+            )
+            
+            Spacer()
+            
+            Text("What is your zodiac sign?")
+                .font(.title)
+            
+            TextField("", text: $zodiacSign)
+                .autocapitalization(.none)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width: 1)
+            
+            Spacer()
+            
+            HStack{
+                Spacer()
+                Button("Submit Info") {
+                    presentAlert = true
+                    
+                }
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(.purple)
+                Spacer()
+            }
+            
         }
         .padding()
+        .alert("Hi \(name)! Thank you for submitting your info!", isPresented: $presentAlert, actions: {})
     }
 }
 
