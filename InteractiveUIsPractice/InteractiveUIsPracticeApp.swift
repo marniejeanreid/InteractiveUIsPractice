9c09ec049ba9875d9f802d097f3f5b0ea72c940a
Marnie Reid <marnie@kodewithklossy.com>
2024-03-25 14:15:50 -0400

    Initial Commit

---
+++ InteractiveUIsPractice/InteractiveUIsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIsPracticeApp.swift
+//  InteractiveUIsPractice
+//
+//  Created by scholar on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
